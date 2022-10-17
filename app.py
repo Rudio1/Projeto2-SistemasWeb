3297003526222c729a98dd46dc958b86eec1870a
Ronaldo Bueno Lopes Junior <ronaldo.junior@extrabom.com.br>
2022-10-17 10:34:05 -0300

    Adicionando iteração entre a tela Login e Cadastro

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
-import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def initial():
-    response = requests.get('https://www.melivecode.com/api/users')
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        pass
 
-    usuarios = response.json()
+    return render_template('Login.html')
 
-    return render_template('Cadastro.html', usuarios=usuarios)
+@app.route('/cadastro', methods=['POST', 'GET'])
+def cadastro():
+    print("oi")
+    return render_template('Cadastro.html')
