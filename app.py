575c551d791645f7a8162decb78ff711135019f7
Ronaldo Bueno Lopes Junior <ronaldo.junior@extrabom.com.br>
2022-10-17 09:31:25 -0300

    Adicionando templates

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def initial():
+    response = requests.get('https://www.melivecode.com/api/users')
+
+    usuarios = response.json()
+
+    return render_template('base.html', usuarios=usuarios)
